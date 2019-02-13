fc2070da837ccec63e7519e1b59929afba5f6ce5
RasputinZA <47171762+RasputinZA@users.noreply.github.com>
2019-02-13 09:30:42 +0200

    Update App.java

--- src/main/java/Test123/App.java
+++ src/main/java/Test123/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hi there colleague!" );
         NewClass test = new NewClass();
         test.sayHello();
     }
